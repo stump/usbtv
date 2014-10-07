99e77313e72cc33f2fe173a9dc3f4ad35c7ecbdd
John Stumpo <stump@stump.io>
2014-10-07 02:21:48 -0400

    usbtv: audio: Use snd_card_new instead of snd_card_create.
    Signed-off-by: John Stumpo <stump@stump.io>

--- usbtv-audio.c
+++ usbtv-audio.c
@@ -330,8 +330,8 @@ int usbtv_audio_init(struct usbtv *usbtv)
 	INIT_WORK(&usbtv->snd_trigger, snd_usbtv_trigger);
 	atomic_set(&usbtv->snd_stream, 0);
 
-	rv = snd_card_create(SNDRV_DEFAULT_IDX1, "usbtv", THIS_MODULE, 0,
-		&card);
+	rv = snd_card_new(usbtv->dev, SNDRV_DEFAULT_IDX1, "usbtv",
+		THIS_MODULE, 0,	&card);
 	if (rv < 0)
 		return rv;
 
